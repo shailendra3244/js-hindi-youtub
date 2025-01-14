084ba955af7c35813dfc6c2af53b5b7857bb861c
shailendra3244 <shailendrapatel3244@gmail.com>
2025-01-14 17:54:09 +0000

    DataTypes

---
+++ 01_basic/DataType.js
@@ -0,0 +1,28 @@
+let Name ='subham'
+let Age =18
+
+let IsLoggedIn ="" 
+// 0=false , 1= true , string= true ,""= false
+
+console.log(typeof "subham");
+console.log(typeof (Age));
+
+let Score ="33"
+console.log(typeof(Score));
+
+let ValueInNumber =Number(Score)
+console.log(ValueInNumber);
+
+console.log(typeof (ValueInNumber));
+let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log( BooleanIsLoggedIn);
+
+let SomeNumber =33
+let StringNumber =String(SomeNumber)
+
+console.log(typeof StringNumber);
+
+
+
+
+
