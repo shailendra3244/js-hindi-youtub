81d0e7be8c95e4ff836b50573ffecad3843a9e3c
shailendra3244 <shailendrapatel3244@gmail.com>
2025-01-14 17:36:57 +0000

    variable done

---
+++ 01_basic/variable.js
@@ -0,0 +1,22 @@
+const accountId=14553
+let  accountEmail="subham@gmail.com"
+var accountPassword="12345"
+let accountCity="jaipur"
+
+let accountState;
+ accountEmail ="shail@gmail.com"
+//  accountId =123
+accountCity ="jabalpur"
+accountPassword ="8989"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log([accountState]);
+// console.log(accountCity);
+
+console.log([accountId,accountCity,accountEmail,accountPassword,accountState,]);
+
+
+
+
