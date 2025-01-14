7adac7291bc2624594900ae8e6e6df18daaff0b2
shailendra3244 <shailendrapatel3244@gmail.com>
2025-01-14 18:33:17 +0000

    DataComparision

---
+++ 01_basic/DataComparision.js
@@ -0,0 +1,77 @@
+// let vallue =3
+// let NegValue=-vallue
+// console.log(NegValue);
+
+// let Str1="subh "
+// let Str2="patel"
+// let Str3=Str1+Str2;
+// console.log( Str3);
+// console.log( (1+'1'));
+// console.log( '1'+1);
+// console.log( "1"+"1");
+// console.log( 1+1);
+// console.log("1"+(2+2));
+// console.log(1+2+'5');
+
+// let GmaeCounter=100
+// GmaeCounter++;
+// console.log(GmaeCounter);
+
+
+
+
+
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log("2">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+console.log( "2"==2);
+
+const Score=100
+const ScoreValue=100.3
+const IsLoggedIn=false
+const OutSideTemp=null
+let UserEmail;
+const Id=Symbol('123')
+const AnotherId=Symbol('123')
+console.log(Id==AnotherId);
+
+
+const Subh=['shatiman','naagraj','krish'];
+let myobj ={
+    name:"hitesh" ,
+    age:25
+}
+console.log(myobj.name);
+console.log(myobj.age);
+console.log(Subh);
+
+const Myfunction=function()
+{
+    console.log("subham patel");
+    
+}
+console.log(Myfunction());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
